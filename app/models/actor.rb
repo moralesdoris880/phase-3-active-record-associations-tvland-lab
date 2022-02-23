d45e8cc551e1f53760a3c361c53257f61b9650a9
Doris Morales <moralesdoris880@gmail.com>
2022-02-23 17:53:38 -0500

    Completed assignment

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,12 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+    def full_name
+        "#{self.first_name} #{self.last_name}"
+    end
+    def list_roles
+        role = Character.find_by(actor_id: self.id)
+        show = Show.find_by(id: role.show_id)
+        "#{role.name} - #{show.name}"
+    end
 end
